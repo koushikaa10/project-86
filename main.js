a4e53472f9f9d1fee0ac21e4e427620714761649
koushikaa10 <85743248+koushikaa10@users.noreply.github.com>
2022-01-05 20:21:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.Image.fromURL('canvas-background.png' , function(Img)
+    {
+        block_image_object = Img;
+        block_image_object.scaleToWidth(700);
+        block_image_object.scaleToHeight(510);
+        block_image_object.set({top:0,left:0});
+        canvas.add(block_img_object);
+    }
+    );
+}
+
+function playSound(){
+	x.play();
+}
